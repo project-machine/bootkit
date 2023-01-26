d28bcad52f75d46af89562f0aaa1939d90061d10
Scott Moser <smoser@brickies.net>
2023-01-26 14:28:44 -0500

    Publish to zot and release.

--- Makefile
+++ Makefile
@@ -10,3 +10,11 @@ build:
 .PHONY: pkg-build
 pkg-build:
 	cd pkg && $(STACKER_BUILD)
+
+.PHONY: publish
+publish:
+	@$(call required_var,PUBLISH_USER)
+	@$(call required_var,PUBLISH_PASSWORD)
+	@$(call required_var,PUBLISH_URL)
+	@echo publishing with $(PUBLISH_USER):SECRET to $(PUBLISH_URL)
+	@$(STACKER_PUBLISH)
--- common.mk
+++ common.mk
@@ -9,7 +9,7 @@ $(error "Bad value for MAIN_VERSION: '$(MAIN_VERSION)'")
 endif
 
 SERIAL = $(shell date --utc "+%y%m%d")
-MAIN_SERIAL = $(MAIN_VERSION)+$(SERIAL)
+MAIN_SERIAL = $(MAIN_VERSION).$(SERIAL)
 
 BUILD_D ?= $(abspath $(TOP_D)/../build-$(NAME))
 STACKER_D ?= $(BUILD_D)/stacker
@@ -21,6 +21,16 @@ OCI_D ?= $(BUILD_D)/oci
 STACKER_OPTS = --stacker-dir=$(STACKER_D) --roots-dir=$(ROOTS_D) --oci-dir=$(OCI_D) $(STACKER_COMMON_OPTS)
 STACKER_BUILD = stacker $(STACKER_OPTS) build $(STACKER_BUILD_ARGS) --layer-type=squashfs $(STACKER_SUBS)
 STACKER_RBUILD = stacker $(STACKER_OPTS) recursive-build $(STACKER_BUILD_ARGS) --search-dir=layers/ --layer-type=squashfs $(STACKER_SUBS)
+STACKER_PUBLISH = stacker $(STACKER_OPTS) publish $(STACKER_BUILD_ARGS) \
+	--search-dir=layers/ --layer-type=squashfs $(STACKER_SUBS) \
+	"--username=$(PUBLISH_USER)" "--password=$(PUBLISH_PASSWORD)" \
+	"--url=$(PUBLISH_URL)" --tag=$(MAIN_SERIAL)
+
+# use this as `$(call required_var,VARNAME)` to assert VARNAME is set.
+# It expands to:
+# [ -n "value-of-$VARNAME" ] || { echo "rule-name ...VARNAME"; exit 1; }
+required_var = [ -n "$(value $1)" ] || { echo "$@ requires environment variable $1"; exit 1; }
+
 
 debug:
 	@echo MAIN_VERSION=$(MAIN_VERSION)
