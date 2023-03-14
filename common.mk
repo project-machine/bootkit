14155049206e67c06d93ba73bad94870a92aafb6
Scott Moser <scmoser@cisco.com>
2023-03-14 07:44:40 -0700

    Workaround stacker issue with single layer-type and build_only
    This works around stacker issue 442
    https://github.com/project-stacker/stacker/issues/442
    
    The result here is that we will build both layer types (tar and
    squashfs) but only publish the squashfs.

--- common.mk
+++ common.mk
@@ -19,8 +19,8 @@ OCI_D ?= $(BUILD_D)/oci
 # STACKER_COMMON_OPTS = --debug
 # STACKER_BUILD_ARGS = --shell-fail
 STACKER_OPTS = --stacker-dir=$(STACKER_D) --roots-dir=$(ROOTS_D) --oci-dir=$(OCI_D) $(STACKER_COMMON_OPTS)
-STACKER_BUILD = stacker $(STACKER_OPTS) build $(STACKER_BUILD_ARGS) --layer-type=squashfs $(STACKER_SUBS)
-STACKER_RBUILD = stacker $(STACKER_OPTS) recursive-build $(STACKER_BUILD_ARGS) --search-dir=layers/ --layer-type=squashfs $(STACKER_SUBS)
+STACKER_BUILD = stacker $(STACKER_OPTS) build $(STACKER_BUILD_ARGS) --layer-type=squashfs --layer-type=tar $(STACKER_SUBS)
+STACKER_RBUILD = stacker $(STACKER_OPTS) recursive-build $(STACKER_BUILD_ARGS) --search-dir=layers/ --layer-type=squashfs --layer-type=tar $(STACKER_SUBS)
 STACKER_PUBLISH = stacker $(STACKER_OPTS) publish $(STACKER_BUILD_ARGS) \
 	--search-dir=layers/ --layer-type=squashfs $(STACKER_SUBS) \
 	"--username=$(PUBLISH_USER)" "--password=$(PUBLISH_PASSWORD)" \
