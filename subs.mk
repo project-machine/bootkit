1a174cea97d2b13a5ae4ad04bc6bd6dd916a5644
Scott Moser <smoser@brickies.net>
2023-07-18 11:33:19 -0400

    Drop unused substitution KEYS_DIR.
    KEYS_DIR was replaced with KEYSET_D, but wasn't removed from
    the subs.mk file.

--- subs.mk
+++ subs.mk
@@ -10,7 +10,6 @@ STACKER_SUBS = \
 	--substitute=DOCKER_BASE=$(DOCKER_BASE) \
 	--substitute=UBUNTU_MIRROR=$(UBUNTU_MIRROR) \
 	--substitute=HOME=$(HOME) \
-	--substitute=KEYS_DIR=$(HOME)/.local/share/machine/trust/keys \
 	--substitute=TOP_D=$(TOP_D) \
 	--substitute=MOSCTL_BINARY=$(MOSCTL_BINARY) \
 	--substitute=ZOT_BINARY=$(ZOT_BINARY)
