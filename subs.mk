4dcbc7756ad091654a1d3737456d9b3be34af9e9
Scott Moser <smoser@brickies.net>
2023-07-18 10:22:01 -0400

    Allow MOSCTL_BINARY and ZOT_BINARY to be read from the environment.
    This allows user that invokes make to influence the mosctl
    and zot binary via MOSCTL_BINARY and ZOT_BINARY variables respectively.
    
    The make variables will take the values present in the environment
    or on the make command line if provided.
    
    So either:
    
        export MOSCTL_BINARY=/tmp/my-mosctl
        export ZOT_BINARY=http://...
    
    or
    
        make MOSCTL_BINARY=... ZOT_BINARY=...
    
    There is a workaround for stacker issue
    https://github.com/project-stacker/stacker/issues/456

--- subs.mk
+++ subs.mk
@@ -2,6 +2,8 @@ KEYSET ?= snakeoil
 DOCKER_BASE ?= docker://
 UBUNTU_MIRROR ?= http://archive.ubuntu.com/ubuntu
 KEYSET_D ?= $(HOME)/.local/share/machine/trust/keys/$(KEYSET)
+MOSCTL_BINARY ?= https://github.com/project-machine/mos/releases/download/0.0.9/mosctl
+ZOT_BINARY ?= https://github.com/project-zot/zot/releases/download/v1.4.3/zot-linux-amd64-minimal
 
 STACKER_SUBS = \
 	--substitute=KEYSET_D=$(KEYSET_D) \
@@ -9,4 +11,6 @@ STACKER_SUBS = \
 	--substitute=UBUNTU_MIRROR=$(UBUNTU_MIRROR) \
 	--substitute=HOME=$(HOME) \
 	--substitute=KEYS_DIR=$(HOME)/.local/share/machine/trust/keys \
-	--substitute=TOP_D=$(TOP_D)
+	--substitute=TOP_D=$(TOP_D) \
+	--substitute=MOSCTL_BINARY=$(MOSCTL_BINARY) \
+	--substitute=ZOT_BINARY=$(ZOT_BINARY)
