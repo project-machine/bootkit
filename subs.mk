abf22e52605f9dcc08b6973c44e462fcf3787044
Serge Hallyn <shallyn@cisco.com>
2023-08-28 16:26:30 -0500

    update zot version
    Signed-off-by: Serge Hallyn <shallyn@cisco.com>

--- subs.mk
+++ subs.mk
@@ -3,7 +3,7 @@ DOCKER_BASE ?= docker://
 UBUNTU_MIRROR ?= http://archive.ubuntu.com/ubuntu
 KEYSET_D ?= $(HOME)/.local/share/machine/trust/keys/$(KEYSET)
 MOSCTL_BINARY ?= https://github.com/project-machine/mos/releases/download/0.0.12/mosctl
-ZOT_BINARY ?= https://github.com/project-zot/zot/releases/download/v1.4.3/zot-linux-amd64-minimal
+ZOT_BINARY ?= https://github.com/project-zot/zot/releases/download/v2.0.0-rc5/zot-linux-amd64-minimal
 
 STACKER_SUBS = \
 	--substitute=KEYSET_D=$(KEYSET_D) \
