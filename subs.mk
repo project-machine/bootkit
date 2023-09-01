e76eafada395caa5bb0fc5ef04d01b99a548fb9c
Serge Hallyn <shallyn@cisco.com>
2023-09-01 14:42:27 -0500

    update mosctl to v0.0.14
    Signed-off-by: Serge Hallyn <shallyn@cisco.com>

--- subs.mk
+++ subs.mk
@@ -2,7 +2,7 @@ KEYSET ?= snakeoil
 DOCKER_BASE ?= docker://
 UBUNTU_MIRROR ?= http://archive.ubuntu.com/ubuntu
 KEYSET_D ?= $(HOME)/.local/share/machine/trust/keys/$(KEYSET)
-MOSCTL_BINARY ?= https://github.com/project-machine/mos/releases/download/v0.0.13/mosctl
+MOSCTL_BINARY ?= https://github.com/project-machine/mos/releases/download/v0.0.14/mosctl
 ZOT_BINARY ?= https://github.com/project-zot/zot/releases/download/v2.0.0-rc5/zot-linux-amd64-minimal
 
 STACKER_SUBS = \
