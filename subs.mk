e59624320b7fccb73849c82d54094ab6330ab91b
Serge Hallyn <serge@hallyn.com>
2024-02-24 21:56:07 -0600

    bump mosctl version
    Signed-off-by: Serge Hallyn <serge@hallyn.com>

--- subs.mk
+++ subs.mk
@@ -2,7 +2,7 @@ KEYSET ?= snakeoil
 DOCKER_BASE ?= docker://
 UBUNTU_MIRROR ?= http://archive.ubuntu.com/ubuntu
 KEYSET_D ?= $(HOME)/.local/share/machine/trust/keys/$(KEYSET)
-MOSCTL_BINARY ?= https://github.com/project-machine/mos/releases/download/v0.0.28/mosctl-linux-amd64
+MOSCTL_BINARY ?= https://github.com/project-machine/mos/releases/download/v0.0.29/mosctl-linux-amd64
 ZOT_BINARY ?= https://github.com/project-zot/zot/releases/download/v2.0.0-rc5/zot-linux-amd64-minimal
 
 STACKER_SUBS = \
